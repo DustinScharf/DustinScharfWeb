393a1785ad2cc88b92bd2cd80729ff195ab5d6b2
Dustin Scharf <d.scharf.2002@gmail.com>
2024-10-26 18:39:52 +0200

    Sidebar in one file

---
+++ WebPage/sidebar.js
@@ -0,0 +1,17 @@
+
+document.getElementById('sidebar').innerHTML = `
+    <ul>
+        <li><a href="projekt1.html">Projekt 1</a></li>
+        <li><a href="projekt2.html">Projekt 2</a></li>
+        <li><a href="projekt3.html">Projekt 3</a></li>
+        <li><a href="projekt4.html">Projekt 4</a></li>
+        <li><a href="projekt5.html">Projekt 5</a></li>
+        <li><a href="projekt6.html">Projekt 6</a></li>
+        <li><a href="projekt7.html">Projekt 7</a></li>
+        <li><a href="projekt8.html">Projekt 8</a></li>
+        <li><a href="projekt9.html">Projekt 9</a></li>
+        <li><a href="projekt10.html">Projekt 10</a></li>
+        <li style="height: 20px;"></li> <!-- Platzhalter -->
+        <li><a href="index.html">Hilfe</a></li>
+    </ul>
+`;
