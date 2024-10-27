e86ae8c945858756f17503655f95dbe12d1ef63c
Dustin Scharf <d.scharf.2002@gmail.com>
2024-10-27 15:45:08 +0100

    Added contents to all sides but not projects

--- WebPage/sidebar.js
+++ WebPage/sidebar.js
@@ -1,6 +1,8 @@
 
 document.getElementById('sidebar').innerHTML = `
     <ul>
+        <li><a href="index.html"><b>Start</b></a></li>
+        <li style="height: 20px;"></li> <!-- Platzhalter -->
         <li><a href="projekt1.html">Projekt 1</a></li>
         <li><a href="projekt2.html">Projekt 2</a></li>
         <li><a href="projekt3.html">Projekt 3</a></li>
@@ -12,6 +14,13 @@ document.getElementById('sidebar').innerHTML = `
         <li><a href="projekt9.html">Projekt 9</a></li>
         <li><a href="projekt10.html">Projekt 10</a></li>
         <li style="height: 20px;"></li> <!-- Platzhalter -->
-        <li><a href="index.html">Hilfe</a></li>
+        <li><a href="hilfe.html"><b>Bin am Ende und brauch Hilfe</b></a></li>
+        <li style="height: 20px;"></li> <!-- Platzhalter -->
+        <li><a href="impressum.html">Impressum</a></li>
+        <li><a href="datenschutz.html">Datenschutz</a></li>
+        <li style="height: 20px;"></li> <!-- Platzhalter -->
+        <li><a href="index.html"><b>DUSTIN SCHARF</b></a></li>
+        <li><a href="mailto:email@kommt.bald"><i>email@kommt.bald</i></a></li>
+<!--        <li><a href="https://instagram.com/dustin.scharf"><i>Instagram</i></a></li>-->
     </ul>
 `;
