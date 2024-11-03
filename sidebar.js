0f075decf3b3d96b95390beab9d1b1b3adb7c6a5
Dustin Scharf <d.scharf.2002@gmail.com>
2024-11-03 01:11:19 +0100

    update

--- sidebar.js
+++ sidebar.js
@@ -3,29 +3,36 @@ document.getElementById('sidebar').innerHTML = `
         <ul style="list-style-type: none; padding: 0; margin-bottom: 60px;">
             <li><a href="index.html" style="font-weight: bold; text-decoration: none;">Start</a></li>
             <li style="height: 20px;"></li> <!-- Platzhalter -->
+            
+            <li><a href="slides.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
+                <span>Präsentation</span>
+                <small style="color: gray; font-size: 12px; margin-top: 5px;">Präsi zum Download</small>
+            </a></li>
+            
+            <li><a href="umfrage.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
+                <span>Interessen Umfrage</span>
+                <small style="color: gray; font-size: 12px; margin-top: 5px;">Für spätere Aufgaben</small>
+            </a></li>
 
+            <li style="height: 20px;"></li> <!-- Platzhalter -->
+            
             <li><a href="projekt1.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
                 <span>Projekt 1</span>
-                <small style="color: gray; font-size: 12px; margin-top: 5px;">Interessen Umfrage</small>
+                <small style="color: gray; font-size: 12px; margin-top: 5px;">ChatGPT nutzen</small>
             </a></li>
 
             <li><a href="projekt2.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
                 <span>Projekt 2</span>
-                <small style="color: gray; font-size: 12px; margin-top: 5px;">ChatGPT nutzen</small>
+                <small style="color: gray; font-size: 12px; margin-top: 5px;">Facharbeit einfach schreiben</small>
             </a></li>
 
             <li><a href="projekt3.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
                 <span>Projekt 3</span>
-                <small style="color: gray; font-size: 12px; margin-top: 5px;">Facharbeit einfach schreiben</small>
+                <small style="color: gray; font-size: 12px; margin-top: 5px;">Zeugnisnote berechnen</small>
             </a></li>
 
             <li><a href="projekt4.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
                 <span>Projekt 4</span>
-                <small style="color: gray; font-size: 12px; margin-top: 5px;">Zeugnisnote berechnen</small>
-            </a></li>
-
-            <li><a href="projekt5.html" style="display: flex; flex-direction: column; align-items: center; padding: 10px; text-decoration: none;">
-                <span>Projekt 5</span>
                 <small style="color: gray; font-size: 12px; margin-top: 5px;">KI easy selbst coden</small>
             </a></li>
 
@@ -39,7 +46,7 @@ document.getElementById('sidebar').innerHTML = `
         <div style="position: sticky; bottom: 0; background-color: white; padding: 10px 10px 25px;text-align: center;;">
             <a href="index.html" style="font-weight: bold; text-decoration: none;">DUSTIN SCHARF</a>
             <br>
-            <small style="color: gray; font-size: 12px; text-decoration: none;">dustin.scharf.info@gmail.com</small>
+            <small style="color: gray; font-size: 12px; text-decoration: none;">help@dustin-scharf.com</small>
             <div style="display: flex; justify-content: center; gap: 10px; margin-top: 5px;">
                 <a href="impressum.html" style="color: gray; font-size: 12px; text-decoration: none;">Impressum</a>
                 <a href="datenschutz.html" style="color: gray; font-size: 12px; text-decoration: none;">Datenschutz</a>
